8667b3a6b6a1a80aa0aba5851edec6616ae9924f
Khanh <tuankhanhbf@gmail.com>
2023-06-09 12:24:27 +0700

    update

--- js/detail.js
+++ js/detail.js
@@ -90,6 +90,38 @@ window.onload = function () {
     document.querySelector(".intro__content").innerHTML = showProduct;
   });
   promise.catch(function (erro) {
-    console.log(erro);
+    console.log("SUn", erro);
+    //     <div class="intro__left">
+    //     <img
+    //       src="${res.data.content.image}"
+    //       alt=""
+    //     />
+    //   </div>
+    //   <div class="intro__right">
+    //     <h3>Product name</h3>
+    //     <p>
+    //       ${res.data.content.name}
+    //     </p>
+    //     <h5 class="size">Size</h5>
+    //     <div class="intro__size">
+    //       <button>36</button>
+    //       <button>37</button>
+    //       <button>38</button>
+    //       <button>39</button>
+    //       <button>40</button>
+    //       <button>41</button>
+    //       <button>42</button>
+    //     </div>
+    //     <h4>${res.data.content.price}$</h4>
+    //     <div class="intro__buy">
+    //       <button><i class="fa-solid fa-plus"></i></button>
+    //       <span>1</span>
+    //       <button><i class="fa-solid fa-minus"></i></button>
+    //     </div>
+    //     <button class="btn_buy">Thêm vào giỏ hàng</button>
+    //     <p id="addThanhCong"></p>
+    //   </div>
+    // </div>`;
+    //     document.querySelector(".intro__content").innerHTML = showErro;
   });
 };
